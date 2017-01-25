8e2e994706f34163134f39cbc06d6e762ed9e907
walterdd <dasha-walter@yandex.ru>
2017-01-25 16:32:44 +0300

    add requirements to setup.py

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,7 @@ FROM boremeter/caffe-opencv
 
 WORKDIR /root
 
-# Install pandas, Jinja2
-RUN pip install update pip && \
-  pip install Jinja2 pandas tqdm
-
 # Clone AuditoryTracking repository
 WORKDIR /root
 RUN git clone https://github.com/walterdd/Boremeter.git && \
   cd Boremeter && python setup.py install
-
--- setup.py
+++ setup.py
@@ -7,8 +7,14 @@ setup(
     version='0.1',
     description='An app for tracking auditory boredom on video',
     author='Ilya Soloviev, Artem Shaforostov, Daria Walter',
-    url='https://github.com/walterdd/Auditory_tracking',
+    url='https://github.com/walterdd/boremeter',
     packages=['boremeter'],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "Jinja2",
+    ],
     entry_points={
           'console_scripts': [
               'boremeter = boremeter.gen_report:main'
